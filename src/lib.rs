105c5a0e5b12b77946721801e4b8c38839e648f6
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-02-28 14:23:18 +0400

    missing_docs_in_private_items lint added

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,11 @@
 
 // Lints
 #![deny(missing_docs, rustdoc::missing_crate_level_docs, rustdoc::broken_intra_doc_links)]
-#![warn(clippy::missing_docs_in_private_items)]
+#![warn(
+    clippy::missing_docs_in_private_items,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc
+)]
 
 mod app;
 mod config;
