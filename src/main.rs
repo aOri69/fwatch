2efd7b03e3cc45aaa8edebfd9f67a9813fea2d73
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-03-04 17:07:20 +0400

    default log = info

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use env_logger::Env;
 use fsync::{App, Config};
 use libc::EXIT_FAILURE;
 
 fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default()).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let config = Config::from_args().unwrap_or_else(|err| {
         eprintln!("Arguments error: {err}");
